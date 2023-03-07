2764d2d532b156d04a494ce625165eb42b7cbb43
iower <iower@tutanota.com>
2023-03-07 23:01:40 +0500

    Maps

--- main.go
+++ main.go
@@ -30,12 +30,60 @@ func main() {
 	} else {
 		fmt.Println(`not even`)
 	}
-	
+
+
 	// structs
 
 	account := Account{}
 	fmt.Printf("Id: %v\n", account.id)
 	fmt.Printf("Balance: $%0.2f\n", account.balance)
+
+
+	// arrays and slices
+
+	xs := [4]int{1, 2, 3, 4}
+
+	for _, value := range xs {
+		fmt.Printf("- %d\n", value)
+	}
+
+	ys := []int{1, 2 , 3, 4, 5}
+
+	for _, value := range ys {
+		fmt.Printf("-- %d\n", value)
+	}
+
+	zs := []int{}
+	zs = append(zs, 1)
+	zs = append(zs, 2)
+	zs = append(zs, 3)
+
+	for _, value := range zs {
+		fmt.Printf("--- %d\n", value)
+	}
+
+	fmt.Printf("\n")
+	zs[1] = 22
+
+	for _, value := range zs {
+		fmt.Printf("--- %d\n", value)
+	}
+
+	fmt.Printf("\n %d \n", zs[1])
+
+
+	// maps
+
+	testmap := map[string]int{
+		"key1": 11,
+		"key2": 22,
+	}
+
+	fmt.Println("value1 =", testmap["key1"])
+
+	for key, value := range testmap {
+		fmt.Printf("%s: %d \n", key, value)
+	}
 }
 
 func isEven(n int) (bool, error) {
